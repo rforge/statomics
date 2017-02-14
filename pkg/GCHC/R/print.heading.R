eeb509bd48ee16ef7f2ffbadda63557b7107cfaf
shenxia <shenxia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-14 06:40:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/statomics@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/GCHC/R/print.heading.R
@@ -0,0 +1,18 @@
+#' Heading Printing Function
+#' 
+#' This function is an auxiliary function for heading printing
+#' 
+#' @return Print out a heading
+#' 
+#' @author Xia Shen
+#' 
+#' 
+print.heading <- function() {
+	cat('*********************************************************************\n')
+	cat('* Genetic Correlation Heterogeneity for Causality (GCHC)\n')
+	cat('* Version 1.0.0\n')
+	cat('* (C) 2017 Xia Shen\n')
+	cat('* Usher Institute of Univ. Edinburgh / MEB of Karolinska Institutet\n')
+	cat('* GNU General Public License v3\n')
+	cat('*********************************************************************\n\n')
+}
